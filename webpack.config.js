3813dbed0439a0ca6d615815490dd35a6825c8d7
Sumitro Chatterjee <sumitrochatterjee@box.com>
2017-12-16 16:56:47 -0800

    babel-loader and preset env. with pre-commit test hook

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-	entry: "./js/index.js",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "bundle.js"
-	},
-	module: {
-		rules: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: "eslint-loader",
-		}]
-	}
-}
+  entry: './js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader',
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['env', {
+              targets: {
+                browsers: ['last 2 version', 'firefox >= 52', 'safari >= 9', 'ie >= 11'],
+              },
+              modules: false,
+            }],
+          ],
+        },
+      },
+    },
+    ],
+  },
+};
